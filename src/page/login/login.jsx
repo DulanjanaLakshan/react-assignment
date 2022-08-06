b61ea8b2793ed98308a3d969528030ae7dc77e61
DulanjanaLakshan <dulanjana20013@gmail.com>
2022-08-06 14:25:54 +0530

    login form

---
+++ src/page/login/login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './login.css'
+
+const LoginForm = () => {
+  return (
+    <div className='main__form'>
+      <div className='box__form'>
+        <div className='box__icon'>
+          <div className='box_icon_img'></div>
+          <h3>Welcome To Login</h3>
+        </div>
+        <div className='box__input'></div>
+        <div className='box__button'></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginForm
